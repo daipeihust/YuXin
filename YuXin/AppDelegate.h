1f1d310a2a52ec0959860e351a9c49db57f44dbe
daipei <948080952@qq.com>
2016-07-12 10:16:46 +0800

    first commit

---
+++ YuXin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YuXin
+//
+//  Created by Dai Pei on 16/7/12.
+//  Copyright © 2016年 Dai Pei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
